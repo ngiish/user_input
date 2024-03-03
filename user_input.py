d6066065f48f0baef908c206b799ac261931bc82
Isaac <govegeku00@gmail.com>
2024-03-04 00:24:30 +0300

    Completed the user_input assignment

---
+++ user_input.py
@@ -0,0 +1,11 @@
+print ("Let's begin coding!!")
+# string = name;
+name= input ("What is your Name?")
+print ("Hello there", name)
+
+age = input ("Please enter your age")
+
+location = input ("Where are you currently staying?")
+
+
+print ("Hello", name, "You are", age, "years old and live in", location)
